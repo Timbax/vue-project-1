51910595f45fde837d285756c718ca2c54e1bd2a
Daniel White <danielrx-8@hotmail.com>
2026-04-07 18:32:24 -0500

    Cambios
    Cambios:
    1. implementacion de ref
    2. implementacion de componentes

---
+++ src/components/ElContador.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ contador }}</h1>
+  <div>
+    
+  </div>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="restar">Restar</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const contador = ref(0);
+const incrementar = ()=>{contador.value++}
+const restar =()=>{contador.value--}
+</script>
+
+<style scoped></style>
