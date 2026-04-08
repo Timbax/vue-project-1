355c0e11722b43900d5215f61946f25c55b49d48
Daniel White <danielrx-8@hotmail.com>
2026-04-07 20:28:28 -0500

    Implementacion de nueva funcion
    Implementación de funcionalidad de agregar datos dentro de un array vacio mediante push en un ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <ElContador />
+  <ListaTareas />
 </template>
 
 <script setup>
 import ElContador from "./components/ElContador.vue";
+import ListaTareas from "./components/ListaTareas.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea"/>
+        <ul>
+            <li v-for="(tareas, index) in tareas" :key="index">{{ tareas }}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const tareas = ref([]);
+const nuevaTarea = ref('');
+
+const addTarea = ()=>{tareas.value.push(nuevaTarea.value)
+    nuevaTarea.value = "";
+}
+</script>
+
+<style scope>
+</style>
