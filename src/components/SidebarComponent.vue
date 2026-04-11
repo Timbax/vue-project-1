cbf5eb9ffba0b4ab0d5306b62c6db5f5395e387c
Daniel White <danielrx-8@hotmail.com>
2026-04-10 21:19:19 -0500

    Fix: Corregir reactividad en SidebarComponent usando computed
    PROBLEMA:
    - watchEffect estaba mal implementado con sintaxis incorrecta
    - No se estaban rastreando correctamente los cambios del store
    - Las propiedades nombre y email no se actualizaban cuando se modificaban en el store
    
    SOLUCIÓN:
    - Cambiar de watchEffect a computed properties
    - computed proporciona reactividad automática directa desde el store
    - Cuando nombre o email cambian en registrarStore, los valores se actualizan automáticamente en el template
    - Código más limpio y eficiente

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sidebar">
-    <h2>Barra lateral</h2>
+    <h2>Bienvenido</h2>
+    <h4>Hola:{{ nombre }}</h4>
+    <h4>Tu Correo es: {{ email }}</h4>
     <ul>
       <li><RouterLink to="/">Inicio</RouterLink></li>
       <li><RouterLink to="/el-contador">Contador</RouterLink></li>
@@ -11,7 +13,15 @@
   </div>
 </template>
 <script setup>
+import { computed } from "vue";
 import { RouterLink } from "vue-router";
+import { useRegistrarStore } from "../modules/registro/stores/registrarStore";
+
+const registrarStore = useRegistrarStore();
+
+// Usando computed para crear referencias reactivas que se actualizan automáticamente
+const nombre = computed(() => registrarStore.nombre);
+const email = computed(() => registrarStore.email);
 </script>
 <style scope>
 .sidebar {
